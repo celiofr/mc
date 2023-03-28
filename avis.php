fd1f29e11cfe4d45cb55a0a5cd7d4d1e70a6c17a
celiofr <129197286+celiofr@users.noreply.github.com>
2023-03-28 20:58:25 +0200

    Delete avis.php

--- avis.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (isset($_POST['nom']) && isset($_POST['avis'])) {
-  $nom = htmlspecialchars($_POST['nom']);
-  $avis = htmlspecialchars($_POST['avis']);
-  $fichier = 'avis.txt';
-  $nouvel_avis = '<div class="avis"><h3>' . $nom . '</h3><
